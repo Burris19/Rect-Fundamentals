55dee4d8051c7f217011909237bec2de1772de8c
Julian <julianhernandez19@gmail.com>
2016-10-04 06:25:22 +0000

    [add] first component

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+
 class App extends React.Component {
   render(){
     return <div>Hello</div>
+    // return React.createElement('h1', null, 'Hello Guys')
   }
 }
+
+// const App = () => <h1>Hello eggheads</h1>
 
 export default App
